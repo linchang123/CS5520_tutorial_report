be8a7a72077140871a7f19b0c6bd95561bbf09d6
Lin Chang <125248171+linchang123@users.noreply.github.com>
2024-12-06 19:20:37 -0800

    Delete tutorial_report directory

--- tutorial_report/script.js
+++
@@ -1,8 +0,0 @@
-function FullView(ImagLink) {
-    document.getElementById("FullImage").src = ImagLink;
-    document.getElementById("FullImageView").style.display = "inline-block";
-}
-
-function CloseFullView() {
-    document.getElementById("FullImageView").style.display = "none";
-}
